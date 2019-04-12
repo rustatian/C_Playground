a51d2bed8ffc346984a61494362b355dcc118042
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-04-12 21:30:49 +0300

    It's 2016; why are we using ColdFusion?!

--- geeksforgeeks/course/main.c
+++ geeksforgeeks/course/main.c
@@ -1,16 +1,34 @@
+#include <stdbool.h>
 #include "stdio.h"
 #include "binary_search.h"
 
 
 int main() {
+    int m;
+    scanf("%d", &m);
+    if (m > 12) {
+        printf("%d", -1);
+        return 0;
+    }
 
-    int arr[] = {5, 6, 7, 8, 9, 10};
-    int n, key;
-    n = sizeof(arr) / sizeof(arr[0]);
-    key = 10;
+    if ((m % 2) == 0) {
+        printf("30");
+        return 0;
+    }
 
-    printf("Index: %d\n", binary_search(arr, 0, n, key));
+    printf("31");
+
+
+
     return 0;
+
+//    int arr[] = {5, 6, 7, 8, 9, 10};
+//    int n, key;
+//    n = sizeof(arr) / sizeof(arr[0]);
+//    key = 10;
+//
+//    printf("Index: %d\n", binary_search(arr, 0, n, key));
+//    return 0;
 }
 
 
