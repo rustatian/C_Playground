5ffb9bec69aa4a3a664c85bbe05b705dbd7b4399
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-05-03 23:06:51 +0300

    I expected something different.

---
+++ c_rem/pointers/main.c
@@ -0,0 +1,5 @@
+//
+// Created by Valery Piashchynski on 2019-05-03.
+//
+
+#include "main.h"
---
+++ c_rem/pointers/main.h
@@ -0,0 +1,8 @@
+//
+// Created by Valery Piashchynski on 2019-05-03.
+//
+
+#ifndef CP_MAIN_H
+#define CP_MAIN_H
+
+#endif //CP_MAIN_H
