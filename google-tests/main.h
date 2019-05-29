2abce964131e406656293c920ff82644243b4957
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-05-29 22:25:08 +0300

    That last commit message about silly mistakes pales in comparision to this one

---
+++ google-tests/main.h
@@ -0,0 +1,12 @@
+#ifndef GTEST_SAMPLES_SAMPLE1_H_
+#define GTEST_SAMPLES_SAMPLE1_H_
+
+#include <stdbool.h>
+
+// Returns n! (the factorial of n).  For negative n, n! is defined to be 1.
+int Factorial(int n);
+
+// Returns true iff n is a prime number.
+bool IsPrime(int n);
+
+#endif  // GTEST_SAMPLES_SAMPLE1_H_
