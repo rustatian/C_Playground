f3fe7bad972714d1729e64e4b7070b8f1c9be2a0
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-09-18 15:19:48 +0300

    It'd be nice if type errors caused the compiler to issue a type error

--- script_linux/recorder.c
+++ script_linux/recorder.c
@@ -14,6 +14,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <stdio.h>
+#include <assert.h>
 #include <stdlib.h>
 
 #define BUF_SIZE 256
@@ -26,6 +27,55 @@ static void ttyReset(void) {
         printf("error while setting origin");
         _exit(1);
     }
+}
+
+#define SWAP_ENDIAN(val) ((unsigned int) ( \
+    (((unsigned int) (val) & (unsigned int) 0x000000ffU) << 24) | \
+    (((unsigned int) (val) & (unsigned int) 0x0000ff00U) <<  8) | \
+    (((unsigned int) (val) & (unsigned int) 0x00ff0000U) >>  8) | \
+    (((unsigned int) (val) & (unsigned int) 0xff000000U) >> 24)))
+
+static int is_little_endian() {
+    static int retval = -1;
+
+    if (retval == -1) {
+        int n = 1;
+        char *p = (char*) &n;
+        char x[] = {1, 0, 0, 0};
+
+        assert(sizeof(int) == 4);
+
+        if (memcmp(p, x, 4) == 0) {
+            retval = 1;
+        } else {
+            retval = 0;
+        }
+    }
+
+    return retval;
+}
+
+static int convert_to_little_endian(int x) {
+    if (is_little_endian()) {
+        return x;
+    } else {
+        return SWAP_ENDIAN(x);
+    }
+}
+
+
+int write_header(FILE *fp, Header *h) {
+    int buf[3];
+
+    buf[0] = convert_to_little_endian(h->tv.tv_sec);
+    buf[1] = convert_to_little_endian(h->tv.tv_usec);
+    buf[2] = convert_to_little_endian(h->len);
+
+    if (fwrite(buf, sizeof(int), 3, fp) == 0) {
+        return 0;
+    }
+
+    return 1;
 }
 
 int main(int argc, char *argv[]) {
@@ -91,15 +141,17 @@ int main(int argc, char *argv[]) {
             exit(1);
         }
 
-        if(FD_ISSET(STDIN_FILENO, &inFds)) { // stdin --> pty
+        ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);
+        ioctl(masterFd, TIOCSWINSZ, &ws);
+
+        Header header;
+
+        if (FD_ISSET(STDIN_FILENO, &inFds)) { // stdin --> pty
             numRead = read(STDIN_FILENO, buf, BUF_SIZE);
             if (numRead <= 0) {
                 printf("success");
                 exit(0);
             }
-
-            ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);
-            ioctl(masterFd, TIOCSWINSZ, &ws);
 
             if (write(masterFd, buf, numRead) != numRead) {
                 printf("fatal, partial");
@@ -107,7 +159,21 @@ int main(int argc, char *argv[]) {
             }
         }
 
-        if(FD_ISSET(masterFd, &inFds)) {
+//        if (FD_ISSET(STDOUT_FILENO, &inFds)) { // stdout --> pty
+//            numRead = read(STDOUT_FILENO, buf, BUF_SIZE);
+//            if (numRead <= 0) {
+//                printf("success");
+//                exit(0);
+//            }
+//
+//            if (write(masterFd, buf, numRead) != numRead) {
+//                printf("fatal, partial");
+//                exit(1);
+//            }
+//        }
+
+
+        if (FD_ISSET(masterFd, &inFds)) {
             numRead = read(masterFd, buf, BUF_SIZE);
             if (numRead <= 0) {
                 if (tcsetattr(masterFd, TCSAFLUSH, &ttyOrigin) == -1) {
@@ -118,10 +184,16 @@ int main(int argc, char *argv[]) {
                 exit(0);
             }
 
+            gettimeofday(&header.tv, NULL);
+
+
             if (write(STDOUT_FILENO, buf, numRead) != numRead) {
                 printf("fatal, partial write");
                 exit(1);
             }
+
+//            write_header(recorderFd, &header);
+
             if (write(recorderFd, buf, numRead) != numRead) {
                 printf("fatal, partial (recorderFd)");
                 exit(1);
--- script_linux/recorder.h
+++ script_linux/recorder.h
@@ -7,6 +7,12 @@
 #include "pty_master_open.h"
 #include "ptyFork.h"
 #include "tty_raw.h"
+#include "sys/time.h"
+
+typedef struct header {
+    struct timeval tv;
+    int len;
+} Header;
 
 #define SCRIPT_LINUX_RECORDER_H
 
