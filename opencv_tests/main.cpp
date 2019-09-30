36d5c281737ebb78cef97887c16174b50123773f
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-09-30 22:31:25 +0300

    first steps

--- opencv_tests/main.cpp
+++ opencv_tests/main.cpp
@@ -2,19 +2,21 @@
 // Created by Valery Piashchynski on 30/9/19.
 //
 
-#include </usr/local/Cellar/opencv/4.1.1_2/include/opencv4/opencv2/imgproc/imgproc.hpp>
-#include </usr/local/Cellar/opencv/4.1.1_2/include/opencv4/opencv2/highgui/highgui.hpp>
-#include </usr/local/Cellar/opencv/4.1.1_2/include/opencv4/opencv2/core/core.hpp>
-#include </usr/local/Cellar/opencv/4.1.1_2/include/opencv4/opencv2/ml/ml.hpp>
-#include </usr/local/Cellar/opencv/4.1.1_2/include/opencv4/opencv2/core/mat.hpp>
-#include <opencv4/opencv.hpp>
+#include "opencv2/core/utility.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
 #include "main.h"
 
 using namespace cv;
 
 int main(int argc, char** argv) {
-    auto img = cv::imread(argv[1], -1);
+    auto img = cv::imread(argv[1], cv::COLOR_BayerBG2BGR);
     if (img.empty())
         return -1;
-    
+
+    cv::namedWindow("Example1", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Example 1", img);
+    cv::waitKey(0);
+    cv::destroyWindow("Example1");
+    return 0;
 }
