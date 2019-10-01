2e38975323737302616e2554b468bfd31be18785
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-10-01 09:27:14 +0300

    this should fix it

---
+++ opencv_tests/video.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Valery Piashchynski on 1/10/19.
+//
+
+#include "video.h"
+
+int main() {
+    
+}
---
+++ opencv_tests/video.h
@@ -0,0 +1,8 @@
+//
+// Created by Valery Piashchynski on 1/10/19.
+//
+
+#ifndef OPENCV_TESTS_VIDEO_H
+#define OPENCV_TESTS_VIDEO_H
+
+#endif //OPENCV_TESTS_VIDEO_H
