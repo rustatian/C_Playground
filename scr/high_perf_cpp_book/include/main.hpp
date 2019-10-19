fc12965007e6465941e16949b53354f0ce077ab1
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-10-19 14:36:25 +0300

    total refactorings

---
+++ scr/high_perf_cpp_book/include/main.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Valery Piashchynski on 10/19/19.
+//
+
+#ifndef PLAYGROUND_MAIN_HPP
+#define PLAYGROUND_MAIN_HPP
+
+
+class main {
+
+};
+
+
+#endif //PLAYGROUND_MAIN_HPP
---
+++ scr/high_perf_cpp_book/src/main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Valery Piashchynski on 10/19/19.
+//
+
+#include "../include/main.hpp"
