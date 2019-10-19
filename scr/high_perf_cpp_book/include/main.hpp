bc9245c48ec770c0ac439a9c4caa493d1a041ec3
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-10-19 22:58:24 +0300

    work in progress

--- scr/high_perf_cpp_book/include/main.hpp
+++ scr/high_perf_cpp_book/include/main.hpp
@@ -16,5 +16,23 @@ private:
     std::set<std::string> toppings_;
 };
 
+class Person {
+public:
+    // age is not allowed to mutate Person object because is declared as const
+    auto age() const { return age_;}
+    // set_age can mutate the Person object
+    auto set_age(int age) {age_ = age;}
 
+private:
+    int age_{};
+};
+
+class Team {
+public:
+    auto& leader() const { return leader_;}
+    auto& leader() { return leader_;}
+
+private:
+    Person leader_{};
+};
 #endif //PLAYGROUND_MAIN_HPP
--- scr/high_perf_cpp_book/src/main.cpp
+++ scr/high_perf_cpp_book/src/main.cpp
@@ -2,12 +2,15 @@
 // Created by Valery Piashchynski on 10/19/19.
 //
 
+#include <vector>
+#include <boost/shared_ptr.hpp>
 #include "../include/main.hpp"
 
 int main(int argc, char *argv[]) {
     auto t = std::set<std::string>{};
     t.insert("salt");
 
+    std::shared_ptr;
     auto a = Bagel{t};
 
     t.insert("pepper");
@@ -16,3 +19,23 @@ int main(int argc, char *argv[]) {
     t.insert("oregano");
 }
 
+auto nonmutating_func(const std::vector<Team> &teams) {
+    auto tot_age = int{0};
+
+    for (const auto &team: teams) {
+        tot_age += team.leader().age();
+    }
+
+    for (auto &team: teams) {
+        // won't compile, because teams is const
+        // team.leader().set_age(20);
+    }
+}
+
+auto mutating_func(std::vector<Team> &teams) {
+    auto tot_age = int{0};
+
+    for (auto &team: teams) {
+        team.leader().set_age(10);
+    }
+}
