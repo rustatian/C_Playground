4d43b0c7f4cbf9a88078fee2d49208a431542bb6
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-10-21 11:02:57 +0300

    refactorings

---
+++ src/time/main.s
@@ -0,0 +1,36 @@
+	.file	"main.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"%d"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$2, -4(%rbp)
+	movl	-4(%rbp), %eax
+	leal	2(%rax), %edx
+	movl	-4(%rbp), %eax
+	addl	$2, %eax
+	imull	%edx, %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 9.2.0"
+	.section	.note.GNU-stack,"",@progbits
