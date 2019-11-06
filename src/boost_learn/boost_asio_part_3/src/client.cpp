8cc390dac44b87883684f03a74e73d871f1bac40
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-11-07 01:38:18 +0300

    I don't know what the hell I was thinking.

--- src/boost_learn/boost_asio_part_3/src/client.cpp
+++ src/boost_learn/boost_asio_part_3/src/client.cpp
@@ -146,9 +146,128 @@ public:
             unsigned int request_id
     ) {
         // prepare the req string
+        std::string request = "EMULATE_LONG_CALC_OP "
+                              + std::to_string(duration_sec)
+                              + "\n";
+
+        std::shared_ptr<Session> session =
+                std::shared_ptr<Session>(new Session(
+                        m_ioc,
+                        raw_ip_address,
+                        port_num,
+                        request,
+                        request_id,
+                        callback));
+
+        // add new session to the list of active session
+        std::unique_lock<std::mutex> lock(m_active_session_guard);
+        m_active_sessions[request_id] = session;
+        lock.lock();
+
+        session->m_sock.async_connect(
+                session->m_ep,
+                [this, session](const boost::system::error_code &ec) {
+                    if (ec.value() != 0) {
+                        session->m_ec = ec;
+                        onRequestComplete(session);
+                        return;
+                    }
+                    std::unique_lock<std::mutex> cancel_lock(session->m_cancel_guard);
+
+                    if (session->m_was_cancelled) {
+                        onRequestComplete(session);
+                        return;
+                    }
+
+                    boost::asio::async_write(session->m_sock,
+                            boost::asio::buffer(session->m_request),
+                            [this, session](const boost::system::error_code &ec,
+                                    std::size_t bytes_transferred) {
+                       if (ec.value() != 0) {
+                           session->m_ec = ec;
+                           onRequestComplete(session);
+                           return;
+                       }
+
+                       std::unique_lock<std::mutex> cancel_lock(session->m_cancel_guard);
+
+                       if(session->m_was_cancelled) {
+                           onRequestComplete(session);
+                           return;
+                       }
+
+                       boost::asio::async_read_until(session->m_sock, session->m_response_buf,
+                               '\n',
+                               [this, session](const boost::system::error_code &ec, std::size_t bytes_transferred) {
+                           if (ec.value() != 0) {
+                               session->m_ec = ec;
+                           } else {
+                               std::istream strm(&session->m_response_buf);
+                               std::getline(strm, session->m_response);
+                           }
+                           onRequestComplete(session);
+                       });
+                    });
+                }
+        );
+
+    }
+
+    void cancelRequest(unsigned int request_id) {
+        std::unique_lock<std::mutex> lock(m_active_session_guard);
+
+        auto it = m_active_sessions.find(request_id);
+        if(it != m_active_sessions.end()) {
+            std::unique_lock<std::mutex> cancel_lock(it->second->m_cancel_guard);
+
+            it->second->m_was_cancelled = true;
+            it->second->m_sock.cancel();
+        }
+    }
+
+    void close() {
+        // destroy work object
+        m_work.reset(NULL);
+
+        // wait for the io thread to exit
+        m_thread->join();
     }
 
 private:
+    void onRequestComplete(std::shared_ptr<Session> session) {
+        // shutting down the connection. This method may fail in case
+        // socket is not connected
+
+        boost::system::error_code ignored_ec;
+
+
+        session->m_sock.shutdown(
+                boost::asio::ip::tcp::socket::shutdown_both,
+                ignored_ec
+        );
+
+        // remove the session from the map of active sessions
+        std::unique_lock<std::mutex> lock(m_active_session_guard);
+
+        auto it = m_active_sessions.find(session->m_id);
+        if (it != m_active_sessions.end()) {
+            m_active_sessions.erase(it);
+        }
+
+
+        lock.unlock();
+
+        boost::system::error_code ec;
+        if (session->m_ec.value() == 0 && session->m_was_cancelled) {
+            ec = boost::asio::error::operation_aborted;
+        } else {
+            ec = session->m_ec;
+        }
+
+        session->m_callback(session->m_id, session->m_response, ec);
+    }
+
+
     boost::asio::io_context m_ioc;
     std::map<int, std::shared_ptr<Session>> m_active_sessions;
     std::mutex m_active_session_guard;
