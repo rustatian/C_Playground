b8e8c68b36411292424ec5864cdf15e06ee687d0
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-11-07 17:28:02 +0300

    I forgot to commit... So here you go.

--- src/boost_learn/boost_asio_part_3/src/client.cpp
+++ src/boost_learn/boost_asio_part_3/src/client.cpp
@@ -15,6 +15,7 @@
 #include <boost/asio/streambuf.hpp>
 #include <boost/core/noncopyable.hpp>
 #include <mutex>
+#include <thread>
 
 // SYNC CLIENT
 class SyncTCPClient {
@@ -132,10 +133,11 @@ struct Session {
 class AsyncTCPClient : public boost::noncopyable {
 public:
     AsyncTCPClient() {
-        m_work = std::make_unique<boost::asio::io_context::work>(m_ioc);
-        m_thread = std::make_unique<std::thread>([this]() {
-            m_ioc.run();
-        });
+        m_work.reset(new boost::asio::io_context::work(m_ios));
+
+        m_thread.reset(new std::thread([this]() {
+            m_ios.run();
+        }));
     }
 
     void emulateLongComputationOp(
@@ -143,82 +145,98 @@ public:
             const std::string &raw_ip_address,
             unsigned short port_num,
             Callback callback,
-            unsigned int request_id
-    ) {
-        // prepare the req string
+            unsigned int request_id) {
+
+        // Preparing the request string.
         std::string request = "EMULATE_LONG_CALC_OP "
                               + std::to_string(duration_sec)
                               + "\n";
 
         std::shared_ptr<Session> session =
-                std::shared_ptr<Session>(new Session(
-                        m_ioc,
-                        raw_ip_address,
-                        port_num,
-                        request,
-                        request_id,
-                        callback));
+                std::shared_ptr<Session>(new Session(m_ios,
+                                                     raw_ip_address,
+                                                     port_num,
+                                                     request,
+                                                     request_id,
+                                                     callback));
 
-        // add new session to the list of active session
-        std::unique_lock<std::mutex> lock(m_active_session_guard);
+        session->m_sock.open(session->m_ep.protocol());
+
+        // Add new session to the list of active sessions so
+        // that we can access it if the user decides to cancel
+        // the corresponding request before it completes.
+        // Because active sessions list can be accessed from
+        // multiple threads, we guard it with a mutex to avoid
+        // data corruption.
+        std::unique_lock<std::mutex>
+                lock(m_active_sessions_guard);
         m_active_sessions[request_id] = session;
-        lock.lock();
+        lock.unlock();
 
-        session->m_sock.async_connect(
-                session->m_ep,
-                [this, session](const boost::system::error_code &ec) {
-                    if (ec.value() != 0) {
-                        session->m_ec = ec;
-                        onRequestComplete(session);
-                        return;
-                    }
-                    std::unique_lock<std::mutex> cancel_lock(session->m_cancel_guard);
+        session->m_sock.async_connect(session->m_ep,
+                                      [this, session](const boost::system::error_code &ec) {
+                                          if (ec.value() != 0) {
+                                              session->m_ec = ec;
+                                              onRequestComplete(session);
+                                              return;
+                                          }
 
-                    if (session->m_was_cancelled) {
-                        onRequestComplete(session);
-                        return;
-                    }
+                                          std::unique_lock<std::mutex>
+                                                  cancel_lock(session->m_cancel_guard);
 
-                    boost::asio::async_write(session->m_sock,
-                            boost::asio::buffer(session->m_request),
-                            [this, session](const boost::system::error_code &ec,
-                                    std::size_t bytes_transferred) {
-                       if (ec.value() != 0) {
-                           session->m_ec = ec;
-                           onRequestComplete(session);
-                           return;
-                       }
+                                          if (session->m_was_cancelled) {
+                                              onRequestComplete(session);
+                                              return;
+                                          }
 
-                       std::unique_lock<std::mutex> cancel_lock(session->m_cancel_guard);
+                                          boost::asio::async_write(session->m_sock,
+                                                                   boost::asio::buffer(session->m_request),
+                                                            [this, session](const boost::system::error_code &ec,
+                                                                            std::size_t bytes_transferred) {
+                                                                if (ec.value() != 0) {
+                                                                    session->m_ec = ec;
+                                                                    onRequestComplete(session);
+                                                                    return;
+                                                                }
 
-                       if(session->m_was_cancelled) {
-                           onRequestComplete(session);
-                           return;
-                       }
+                                                                std::unique_lock<std::mutex>
+                                                                        cancel_lock(session->m_cancel_guard);
 
-                       boost::asio::async_read_until(session->m_sock, session->m_response_buf,
-                               '\n',
-                               [this, session](const boost::system::error_code &ec, std::size_t bytes_transferred) {
-                           if (ec.value() != 0) {
-                               session->m_ec = ec;
-                           } else {
-                               std::istream strm(&session->m_response_buf);
-                               std::getline(strm, session->m_response);
-                           }
-                           onRequestComplete(session);
-                       });
-                    });
-                }
-        );
+                                                                if (session->m_was_cancelled) {
+                                                                    onRequestComplete(session);
+                                                                    return;
+                                                                }
 
-    }
+                                                                boost::asio::async_read_until(session->m_sock,
+                                                                                       session->m_response_buf,
+                                                                                       '\n',
+                                                                                       [this, session](
+                                                                                               const boost::system::error_code &ec,
+                                                                                               std::size_t bytes_transferred) {
+                                                                                           if (ec.value() != 0) {
+                                                                                               session->m_ec = ec;
+                                                                                           } else {
+                                                                                               std::istream strm(
+                                                                                                       &session->m_response_buf);
+                                                                                               std::getline(strm,
+                                                                                                            session->m_response);
+                                                                                           }
 
+                                                                                           onRequestComplete(session);
+                                                                                       });
+                                                            });
+                                      });
+    };
+
+    // Cancels the request.
     void cancelRequest(unsigned int request_id) {
-        std::unique_lock<std::mutex> lock(m_active_session_guard);
+        std::unique_lock<std::mutex>
+                lock(m_active_sessions_guard);
 
         auto it = m_active_sessions.find(request_id);
-        if(it != m_active_sessions.end()) {
-            std::unique_lock<std::mutex> cancel_lock(it->second->m_cancel_guard);
+        if (it != m_active_sessions.end()) {
+            std::unique_lock<std::mutex>
+                    cancel_lock(it->second->m_cancel_guard);
 
             it->second->m_was_cancelled = true;
             it->second->m_sock.cancel();
@@ -226,57 +244,112 @@ public:
     }
 
     void close() {
-        // destroy work object
+        // Destroy work object. This allows the I/O thread to
+        // exits the event loop when there are no more pending
+        // asynchronous operations.
         m_work.reset(NULL);
 
-        // wait for the io thread to exit
+        // Wait for the I/O thread to exit.
         m_thread->join();
     }
 
 private:
     void onRequestComplete(std::shared_ptr<Session> session) {
-        // shutting down the connection. This method may fail in case
-        // socket is not connected
-
+        // Shutting down the connection. This method may
+        // fail in case socket is not connected. We don’t care
+        // about the error code if this function fails.
         boost::system::error_code ignored_ec;
-
 
         session->m_sock.shutdown(
                 boost::asio::ip::tcp::socket::shutdown_both,
-                ignored_ec
-        );
+                ignored_ec);
 
-        // remove the session from the map of active sessions
-        std::unique_lock<std::mutex> lock(m_active_session_guard);
+        // Remove session form the map of active sessions.
+        std::unique_lock<std::mutex>
+                lock(m_active_sessions_guard);
 
         auto it = m_active_sessions.find(session->m_id);
-        if (it != m_active_sessions.end()) {
+        if (it != m_active_sessions.end())
             m_active_sessions.erase(it);
-        }
-
 
         lock.unlock();
 
         boost::system::error_code ec;
-        if (session->m_ec.value() == 0 && session->m_was_cancelled) {
+
+        if (session->m_ec.value() == 0 && session->m_was_cancelled)
             ec = boost::asio::error::operation_aborted;
-        } else {
+        else
             ec = session->m_ec;
-        }
 
-        session->m_callback(session->m_id, session->m_response, ec);
-    }
+        // Call the callback provided by the user.
+        session->m_callback(session->m_id,
+                            session->m_response, ec);
+    };
 
-
-    boost::asio::io_context m_ioc;
+private:
+    boost::asio::io_context m_ios;
     std::map<int, std::shared_ptr<Session>> m_active_sessions;
-    std::mutex m_active_session_guard;
+    std::mutex m_active_sessions_guard;
     std::unique_ptr<boost::asio::io_context::work> m_work;
     std::unique_ptr<std::thread> m_thread;
 };
 
+void handler(unsigned int request_id, const std::string &response, const boost::system::error_code &ec) {
+    if (ec.value() == 0) {
+        std::cout << "Request #" << request_id
+                  << " has completed. Response: "
+                  << response << std::endl;
+    } else if (ec == boost::asio::error::operation_aborted) {
+        std::cout << "Request #" << request_id
+                  << " has been cancelled by the user."
+                  << std::endl;
+    } else {
+        std::cout << "Request #" << request_id
+                  << " failed! Error code = " << ec.value()
+                  << ". Error message = " << ec.message()
+                  << std::endl;
+    }
+
+    return;
+}
+
+int async_client_invoke() {
+    try {
+        AsyncTCPClient client;
+
+        // Here we emulate the user's behavior.
+
+        // User initiates a request with id 1.
+        client.emulateLongComputationOp(10, "127.0.0.1", 3333,
+                                        handler, 1);
+        // Then does nothing for 5 seconds.
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+        // Then initiates another request with id 2.
+        client.emulateLongComputationOp(11, "127.0.0.1", 3334,
+                                        handler, 2);
+        // Then decides to cancel the request with id 1.
+        client.cancelRequest(1);
+        // Does nothing for another 6 seconds.
+        std::this_thread::sleep_for(std::chrono::seconds(6));
+        // Initiates one more request assigning ID3 to it.
+        client.emulateLongComputationOp(12, "127.0.0.1", 3335,
+                                        handler, 3);
+        // Does nothing for another 15 seconds.
+        std::this_thread::sleep_for(std::chrono::seconds(15));
+        // Decides to exit the application.
+        client.close();
+    }
+    catch (boost::system::system_error &e) {
+        std::cout << "Error occured! Error code = " << e.code()
+                  << ". Message: " << e.what();
+
+        return e.code().value();
+    }
+
+    return 0;
+}
+
 
 int main() {
-
-
+    async_client_invoke();
 }
