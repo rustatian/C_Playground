66002b04646a564f31bf5e5718ed0dcf6febcf42
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-11-09 23:14:22 +0300

    bump to 0.0.3-dev:wq

---
+++ src/basics/include/templates.hpp
@@ -0,0 +1,8 @@
+//
+// Created by valery on 08/11/2019.
+//
+
+#ifndef PLAYGROUND_TEMPLATES_HPP
+#define PLAYGROUND_TEMPLATES_HPP
+
+#endif //PLAYGROUND_TEMPLATES_HPP
---
+++ src/basics/src/templates.cpp
@@ -0,0 +1,70 @@
+//
+// Created by valery on 08/11/2019.
+//
+
+#include <string>
+#include "../include/templates.hpp"
+
+
+//int max(int a, int b) {
+//    if (a > b)
+//        return a;
+//    return b;
+//}
+//
+//double max(double a, double b) {
+//    if (a > b)
+//        return a;
+//    return b;
+//}
+
+template<typename T>
+T max(T a, T b) {
+    if (a > b)
+        return a;
+    return b;
+}
+
+struct EUBankAccount {
+    std::string IBAN;
+    int amount;
+};
+
+struct UKBankAccount {
+    std::string sortNumber;
+    std::string accountNumber;
+    int amount;
+};
+
+template<typename BankAccount>
+
+int getMaxAmount(const BankAccount &a, const BankAccount &b) {
+    if (a.amount > b.amount) {
+        return a.amount;
+    }
+    return b.amount;
+}
+
+///////////////////
+template<typename T>
+T createFrom(const T &other) {
+    return T(other);
+}
+
+int main(int, char **) {
+    max(3, 4);
+    max(3.4, 3.5);
+    max<int>(2, 4);
+
+    EUBankAccount euacc1{"IBAN1", 1000};
+    EUBankAccount euacc2{"IBAN2", 2000};
+
+    getMaxAmount(euacc1, euacc2);
+
+    UKBankAccount ukacc1{"IBAN3", "accnum1", 4000};
+    UKBankAccount ukacc2{"IBAN4", "accnum2", 4000};
+
+    getMaxAmount(ukacc2, ukacc1);
+
+
+}
