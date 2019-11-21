31f44febfd94b0a052f8dd1f28a7d8fcf2684500
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-11-21 22:53:51 +0300

    It only compiles every 5 tries... good luck.

---
+++ src/own_tools/walker/include/Walker.hpp
@@ -0,0 +1,37 @@
+//
+// Created by valery on 20/10/2019.
+//
+
+#ifndef PLAYGROUND_WALKER_HPP
+#define PLAYGROUND_WALKER_HPP
+
+
+#include <string>
+#include <utility>
+#include <vector>
+
+class Walker {
+public:
+    Walker() = default;;
+
+    Walker &operator()() = delete;
+
+    void add_path(const std::string& path);
+
+    int pull_repo() noexcept(false);
+
+    int push_origin() noexcept(false);
+
+    ~Walker() {
+        paths_.clear();
+        paths_.shrink_to_fit();
+    }
+
+private:
+    std::vector<std::string> paths_;
+    const std::string PULL_COMMAND_ = "git pull";
+    const std::string PUSH_COMMAND_ = "git add . && git commit -m \"$(curl -s http://whatthecommit.com/index.txt)\" && git push";
+};
+
+
+#endif //PLAYGROUND_WALKER_HPP
