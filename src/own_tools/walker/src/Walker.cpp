3c689576452719562ed3149df356daca9c312732
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-11-21 23:05:56 +0300

    Ugh. Bad rebase.

--- src/own_tools/walker/src/Walker.cpp
+++ src/own_tools/walker/src/Walker.cpp
@@ -32,33 +32,6 @@ public:
     thread_guard &operator=(thread_guard const &) = delete; // operator = that returns a reference
 };
 
-int main(int argc, char *argv[]) {
-    // usage:
-    // firs arg is dir with repos
-    // second - pull or push
-    // third - push or pull
-    // both or just one
-    if (argc < 2) {
-        std::cerr << "Not enough args";
-        return 1;
-    }
-
-    if (argc > 2) {
-        throw std::runtime_error("wrong number of parameters");
-    }
-
-    Walker w{};
-
-    for (const std::filesystem::directory_entry &entry: std::filesystem::directory_iterator(argv[1])) {
-        if (entry.is_directory()) {
-            w.add_path(entry.path().string());
-        }
-    }
-
-    w.pull_repo();
-    w.push_origin();
-}
-
 
 void Walker::pull_repo() noexcept(false) {
     for (auto &path : paths_){
---
+++ src/own_tools/walker/src/main.cpp
@@ -0,0 +1,34 @@
+//
+// Created by valery on 11/21/19.
+//
+
+#include <iostream>
+#include <filesystem>
+#include "../include/Walker.hpp"
+
+int main(int argc, char *argv[]) {
+    // usage:
+    // firs arg is dir with repos
+    // second - pull or push
+    // third - push or pull
+    // both or just one
+    if (argc < 2) {
+        std::cerr << "Not enough args";
+        return 1;
+    }
+
+    if (argc > 2) {
+        throw std::runtime_error("wrong number of parameters");
+    }
+
+    Walker w{};
+
+    for (const std::filesystem::directory_entry &entry: std::filesystem::directory_iterator(argv[1])) {
+        if (entry.is_directory()) {
+            w.add_path(entry.path().string());
+        }
+    }
+
+    w.pull_repo();
+    w.push_origin();
+}
