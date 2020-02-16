26a03cbe05dfdee8d88ba1c2856ded024b6046e0
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-02-16 16:50:00 +0300

    Fix the fixes

--- src/php_ffi/src/main.cpp
+++ src/php_ffi/src/main.cpp
@@ -2,10 +2,20 @@
 // Created by valery on 16/02/2020.
 //
 
-#include "../include/main.hpp"
-#include <iostream>
+
+#include <cstdint>
+
+extern "C" int32_t fib(int32_t n) {
+    if ((n == 1) || (n == 2)) {
+        return 1;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+}
 
 int main() {
-    std::cout << "heello" << std::endl;
+    fib(12);
 }
+
+
 
