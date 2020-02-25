fb0f8eb0bff93a4c5e01c8e6e188a3d7c84fa00d
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-02-25 08:55:46 +0300

    Reset error count between rows. herpderp

--- src/php_ffi/src/main.cpp
+++ src/php_ffi/src/main.cpp
@@ -2,36 +2,29 @@
 // Created by valery on 16/02/2020.
 //
 
-
-#include <cstdint>
 #include <ImageMagick-7/Magick++.h>
-#include <ImageMagick-7/Magick++/Color.h>
 #include <ImageMagick-7/Magick++/Geometry.h>
 #include <ImageMagick-7/Magick++/Exception.h>
 #include <iostream>
 
+int main(int argc, char **argv) {
+    Magick::InitializeMagick(*argv);
 
-using namespace Magick;
-int main(int argc,char **argv)
-{
-    InitializeMagick(*argv);
- 
     // Construct the image object. Seperating image construction from the
     // the read operation ensures that a failure to read the image file
     // doesn't render the image object useless.
-    Image image;
+    Magick::Image image;
     try {
         // Read a file into image object
-        image.read( "girl.gif" );
+        image.read("girl.gif");
 
         // Crop the image to specified size (width, height, xOffset, yOffset)
-        image.crop( Geometry(100,100, 100, 100) );
+        image.crop(Magick::Geometry(100, 100, 100, 100));
 
         // Write the image to a file
-        image.write( "x.gif" );
+        image.write("x.gif");
     }
-    catch( Exception &error_ )
-    {
+    catch (Magick::Exception &error_) {
         std::cout << "Caught exception: " << error_.what() << std::endl;
         return 1;
     }
