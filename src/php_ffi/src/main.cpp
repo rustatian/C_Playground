c0c008d85b490e01fb72f1b87e51e63f745aed71
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-02-25 22:23:54 +0300

    Herp derp I left the debug in there and forgot to reset errors.

--- src/php_ffi/src/main.cpp
+++ src/php_ffi/src/main.cpp
@@ -3,32 +3,50 @@
 //
 
 #include <ImageMagick-7/Magick++.h>
-#include <ImageMagick-7/Magick++/Geometry.h>
 #include <ImageMagick-7/Magick++/Exception.h>
 #include <iostream>
 
 int main(int argc, char **argv) {
-    Magick::InitializeMagick(*argv);
+    Magick::InitializeMagick(nullptr);
 
-    // Construct the image object. Seperating image construction from the
-    // the read operation ensures that a failure to read the image file
-    // doesn't render the image object useless.
+    int *a = new int();
+    void *b = reinterpret_cast<void *>(a);
+}
+
+extern "C" void resizeByPath(const char *path, const char *dimensions, const char *save_path) {
+    Magick::InitializeMagick(nullptr);
     Magick::Image image;
+
     try {
-        // Read a file into image object
-        image.read("girl.gif");
-
-        // Crop the image to specified size (width, height, xOffset, yOffset)
-        image.crop(Magick::Geometry(100, 100, 100, 100));
-
-        // Write the image to a file
-        image.write("x.gif");
+        image.read(path);
+        image.adaptiveResize(dimensions);
+        image.write(save_path);
+    } catch (Magick::Exception &err) {
+        std::cout << "EXCEPTION: " << err.what() << std::endl;
     }
-    catch (Magick::Exception &error_) {
-        std::cout << "Caught exception: " << error_.what() << std::endl;
+}
+
+extern "C" void resizeByBlob(const char *data, int data_len, const char *dimensions, const char *save_path) {
+    Magick::InitializeMagick(nullptr);
+    Magick::Blob blob(data, data_len);
+    Magick::Image image(blob);
+
+    try {
+        image.adaptiveResize(dimensions);
+        image.adaptiveResize(save_path);
+    } catch (Magick::Exception &err) {
+        std::cout << "EXCEPTION: " << err.what() << std::endl;
+    }
+}
+
+extern "C" int fib(int n) {
+    if ((n == 1) || (n == 2)) {
         return 1;
     }
-    return 0;
+
+    return fib(n - 1) + fib(n - 2);
 }
+
+
 
 
