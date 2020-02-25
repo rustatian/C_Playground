bf41996c490a4e6f68767848da80132934c7715a
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-02-25 08:53:49 +0300

    I transformed a bug into a feature. Once you learn how, you'll never forget it

--- src/php_ffi/src/main.cpp
+++ src/php_ffi/src/main.cpp
@@ -4,18 +4,38 @@
 
 
 #include <cstdint>
+#include <ImageMagick-7/Magick++.h>
+#include <ImageMagick-7/Magick++/Color.h>
+#include <ImageMagick-7/Magick++/Geometry.h>
+#include <ImageMagick-7/Magick++/Exception.h>
+#include <iostream>
 
-extern "C" int32_t fib(int32_t n) {
-    if ((n == 1) || (n == 2)) {
+
+using namespace Magick;
+int main(int argc,char **argv)
+{
+    InitializeMagick(*argv);
+
+    // Construct the image object. Seperating image construction from the
+    // the read operation ensures that a failure to read the image file
+    // doesn't render the image object useless.
+    Image image;
+    try {
+        // Read a file into image object
+        image.read( "girl.gif" );
+
+        // Crop the image to specified size (width, height, xOffset, yOffset)
+        image.crop( Geometry(100,100, 100, 100) );
+
+        // Write the image to a file
+        image.write( "x.gif" );
+    }
+    catch( Exception &error_ )
+    {
+        std::cout << "Caught exception: " << error_.what() << std::endl;
         return 1;
     }
-
-    return fib(n - 1) + fib(n - 2);
+    return 0;
 }
-
-int main() {
-    fib(12);
-}
-
 
 
