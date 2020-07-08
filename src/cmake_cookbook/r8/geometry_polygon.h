0ce6ed26895567c42e678740610f2d03c953367d
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-07-09 01:27:25 +0300

    oops - thought I got that one.

---
+++ src/cmake_cookbook/r8/geometry_polygon.h
@@ -0,0 +1,11 @@
+#ifndef R8_GEOMETRY_POLYGON_H
+#define R8_GEOMETRY_POLYGON_H
+
+namespace geometry {
+    namespace area {
+        double polygon(int nSides, double side);
+        }
+    }
+}
+
+#endif //R8_GEOMETRY_POLYGON_H
