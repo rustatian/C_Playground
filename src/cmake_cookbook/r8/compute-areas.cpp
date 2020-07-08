0168570e8d3af5ab8df4a2ac64dc0e3317ab8d02
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-07-09 01:24:32 +0300

    just checking if git is working properly...

---
+++ src/cmake_cookbook/r8/compute-areas.cpp
@@ -0,0 +1,1 @@
+#include "compute-areas.h"
---
+++ src/cmake_cookbook/r8/geometry_circle.h
@@ -0,0 +1,11 @@
+#ifndef R8_GEOMETRY_CIRCLE_H
+#define R8_GEOMETRY_CIRCLE_H
+
+
+namespace geometry {
+    namespace area {
+        double circle(double area);
+    }
+}
+
+#endif //R8_GEOMETRY_CIRCLE_H
