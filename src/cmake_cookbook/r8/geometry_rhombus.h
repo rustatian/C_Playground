ec592e52afe7ab02955c84d6cf4a0dc7e5069d0f
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-07-09 01:32:25 +0300

    I know what I am doing. Trust me.

---
+++ src/cmake_cookbook/r8/geometry_rhombus.h
@@ -0,0 +1,8 @@
+#ifndef R8_GEOMETRY_RHOMBUS_H
+#define R8_GEOMETRY_RHOMBUS_H
+namespace geometry {
+    namespace area {
+        double rhombus(double d1, double d2);
+    }
+}
+#endif //R8_GEOMETRY_RHOMBUS_H
