4ddde43cfb94f5b298ee56e921fd6864cfe27f2c
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-07-09 01:34:39 +0300

    We Had To Use Dark Magic To Make This Work

---
+++ src/cmake_cookbook/r8/geometry_square.cpp
@@ -0,0 +1,11 @@
+#include "geometry_square.h"
+
+#include <cmath>
+
+namespace geometry {
+    namespace area {
+        double square(double l) {
+            return std::pow(l, 2);
+        }
+    }
+}
---
+++ src/cmake_cookbook/r8/geometry_square.h
@@ -0,0 +1,8 @@
+#ifndef R8_GEOMETRY_SQUARE_H
+#define R8_GEOMETRY_SQUARE_H
+namespace geometry {
+    namespace area {
+        double square(double l);
+    }
+}
+#endif //R8_GEOMETRY_SQUARE_H
