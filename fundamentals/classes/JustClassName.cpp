92d2179919ac20c944eed9a156c8dc74974ba758
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-07-09 11:20:29 +0300

    Oh no

---
+++ fundamentals/classes/JustClassName.cpp
@@ -0,0 +1,18 @@
+#include "JustClassName.h"
+
+class JustClassName {
+private:
+    int privateDataMember;
+
+    int privateMemberFunction();
+
+protected:
+    float protectedDataMember;
+
+    float protectedMemberFunction();
+
+public:
+    double publicDataMember;
+
+    double publicMemberFunction();
+};
