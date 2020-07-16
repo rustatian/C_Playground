db8e0a8b508517b84f160e4cdf9e2e69dd8d9c24
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-07-16 22:10:12 +0300

    Make that it works in 90% of the cases.  3:30.

--- fundamentals/deducing_types/main.cpp
+++ fundamentals/deducing_types/main.cpp
@@ -2,9 +2,23 @@
 #include "main.h"
 
 template<typename T>
-void f(ParamType param);
+void f(T &param); // param is reference
 
 int main() {
+    int x = 27; // x is an int
+    const int cx = x; // cx is a const int
+    const int &rx = x; // rx is a reference to x as a const int
+
+    f(x); // T is int, param's type is int&
+    f(cx); // T is const int, param's type is const int&
+    f(rx); // T is const int, param's type is const int&
 
     return EXIT_SUCCESS;
 }
+
+/*
+ * template<typename T>
+ * void f(ParamType param);
+ *
+ * f(expr); // <----- deduce T and ParamType from expr
+ */
