00d932b562fca317321a482095a3366a14bb253c
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-07-17 22:15:54 +0300

    Made it to compile...

--- fundamentals/deducing_types/main.cpp
+++ fundamentals/deducing_types/main.cpp
@@ -4,6 +4,15 @@
 template<typename T>
 void f(T &param); // param is reference
 
+template<typename T>
+void f2(const T &param);
+
+template<typename T>
+void f3(T *param); // param now is a pointer
+
+template<typename T>
+void f4(T &&param); // universal reference
+
 int main() {
     int x = 27; // x is an int
     const int cx = x; // cx is a const int
@@ -12,6 +21,21 @@ int main() {
     f(x); // T is int, param's type is int&
     f(cx); // T is const int, param's type is const int&
     f(rx); // T is const int, param's type is const int&
+
+
+    int y = 27; // T is int, param's type is const int&
+    const int cy = x; // T is int, param's type is const int&
+    const int &ry = x; // T is int, param's type is const int&
+
+    f2(y);
+    f2(cy);
+    f2(ry);
+
+    int z = 27;
+    const int *zy = &x;
+
+    f3(&x);
+    f3(zy);
 
     return EXIT_SUCCESS;
 }
