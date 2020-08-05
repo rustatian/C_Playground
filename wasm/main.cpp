a100e4ded31c1b86df2d797ff3f07c86e000d770
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-08-05 22:17:36 +0300

    Does anyone read this? I'll be at the coffee shop accross the street.

--- wasm/main.cpp
+++ wasm/main.cpp
@@ -3,14 +3,18 @@
 #include <iostream>
 #include <emscripten/emscripten.h>
 
-int main(int argc, char **argv) {
-    printf("Hello World\n");
-}
-
+EM_JS(void, call_alert,(),{
+alert('hello world!');
+throw 'all done';
+})
 
 extern "C" {
 void EMSCRIPTEN_KEEPALIVE
 myFunction(int argc, char **argv) {
-    std::cout << "MyFunction Called" << std::endl;
+    emscripten_run_script("alert('hi')");
 }
+}
+
+int main(int argc, char **argv) {
+    //call_alert();
 }
