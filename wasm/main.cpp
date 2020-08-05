ca9135c0d256afb506d5dd840d3d4512170bccc9
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-08-05 13:29:58 +0300

    Please forgive me

--- wasm/main.cpp
+++ wasm/main.cpp
@@ -1,19 +1,16 @@
 #include "main.h"
-#include <stdio.h>
+#include <cstdio>
+#include <iostream>
 #include <emscripten/emscripten.h>
 
-int main(int argc, char ** argv) {
+int main(int argc, char **argv) {
     printf("Hello World\n");
 }
 
-#ifdef __cplusplus
+
 extern "C" {
-#endif
-
-void EMSCRIPTEN_KEEPALIVE myFunction(int argc, char ** argv) {
-    printf("MyFunction Called\n");
+void EMSCRIPTEN_KEEPALIVE
+myFunction(int argc, char **argv) {
+    std::cout << "MyFunction Called" << std::endl;
 }
-
-#ifdef __cplusplus
 }
-#endif
