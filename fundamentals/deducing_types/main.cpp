2b772073790e7e3ea1ea413a8b3177273b20ab81
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-08-16 00:54:45 +0300

    By works, I meant 'doesnt work'.  Works now..

--- fundamentals/deducing_types/main.cpp
+++ fundamentals/deducing_types/main.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <iostream>
 #include "main.h"
+#include <concepts>
 
 template<typename T>
 void f(T &param); // param is reference
@@ -14,51 +15,45 @@ void f3(T *param); // param now is a pointer
 template<typename T>
 void f4(T &&param); // universal reference
 
-template<typename T, int N>
-struct Buffer {
-    using value_type = T;
+using namespace std::literals;
 
-    constexpr int size() {
-        return N;
-    }
-};
-
+// Declaration of the concept "Hashable", which is satisfied by
+// any type 'T' such that for values 'a' of type 'T',
+// the expression std::hash<T>{}(a) compiles and its result is convertible to std::size_t
 template<typename T>
-concept EqualityComparable = requires(T a, T b) {
-    {a == b} -> std::boolean;
-    {a != b} -> std::boolean;
+concept Hashable = requires(T a) {
+    { std::hash<T>{}(a) } -> std::convertible_to<std::size_t>;
 };
 
-void f(const EqualityComparable auto&);
+struct meow {};
+
+template<Hashable T>
+void ff(T); // constrained C++20 function template
 
 int main() {
-    Buffer<char, 128> glob;
-    auto a = glob.size();
-    auto b = glob.size();
-    std::cout << a << std::endl;
     int x = 27; // x is an int
     const int cx = x; // cx is a const int
     const int &rx = x; // rx is a reference to x as a const int
 
-    f(x); // T is int, param's type is int&
-    f(cx); // T is const int, param's type is const int&
-    f(rx); // T is const int, param's type is const int&
-
-
-    int y = 27; // T is int, param's type is const int&
-    const int cy = x; // T is int, param's type is const int&
-    const int &ry = x; // T is int, param's type is const int&
-
-    f2(y);
-    f2(cy);
-    f2(ry);
-
-    int z = 27;
-    const int *zy = &x;
-
-    f3(&x);
-    f3(zy);
-
+//    f(x); // T is int, param's type is int&
+//    f(cx); // T is const int, param's type is const int&
+//    f(rx); // T is const int, param's type is const int&
+//
+//
+//    int y = 27; // T is int, param's type is const int&
+//    const int cy = x; // T is int, param's type is const int&
+//    const int &ry = x; // T is int, param's type is const int&
+//
+//    f2(y);
+//    f2(cy);
+//    f2(ry);
+//
+//    int z = 27;
+//    const int *zy = &x;
+//
+//    f3(&x);
+//    f3(zy);
+//
     return EXIT_SUCCESS;
 }
 
