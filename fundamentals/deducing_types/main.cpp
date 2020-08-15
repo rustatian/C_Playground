78fe4e64868d4c9bd858ed4f65ce7926ba0aea19
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-08-15 22:45:57 +0300

    curl -s http://whatthecommit.com/index.txt

--- fundamentals/deducing_types/main.cpp
+++ fundamentals/deducing_types/main.cpp
@@ -24,6 +24,7 @@ struct Buffer {
 
 int main() {
     Buffer<char, 128> glob;
+    glob.size();
     int x = 27; // x is an int
     const int cx = x; // cx is a const int
     const int &rx = x; // rx is a reference to x as a const int
